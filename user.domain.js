93d52aab43980e866772349da470aa6eba5ad9fd
Namita Malik <namita1990@gmail.com>
2016-05-09 14:45:44 +0530

    Adding validations to user schema

--- user.domain.js
+++ user.domain.js
@@ -4,11 +4,11 @@
 
 var mongoose = require('mongoose');
 var User = mongoose.model('User', {
-    firstName: String,
+    firstName: {type: String, required: [true, 'First Name is mandatory field.']},
     lastName: String,
-    userName: String,
-    email: String,
-    mobNumber: String,
-    age: Number
+    userName: {type: String, required: true},
+    email: {type: String, required: true},
+    mobNumber: {type: String, required: true},
+    age: {type: Number, min: [18, 'Minimum age should be 18'], max: [35, 'Maximum age should be 35']}
 });
 module.exports = User;
